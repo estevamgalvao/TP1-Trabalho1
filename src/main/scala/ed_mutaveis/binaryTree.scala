5fc6a5ef92e4a59dc0d1f959487ab740a3a0b115
Estevam Galvão Albuquerque <estevamgalvao1@gmail.com>
2018-05-04 00:52:57 -0300

    update/fix remove bTree

--- src/main/scala/ed_mutaveis/binaryTree.scala
+++ src/main/scala/ed_mutaveis/binaryTree.scala
@@ -9,12 +9,12 @@ class binaryTree[T](func: (T, T) => Boolean) extends traitBinaryTree[T] {
   private var _size: Int = 0
   private var height: Int = 0
 
-  def findLeaf(value: T, mode: Int): nodeTree[T] = {
+  def findLeaf(value: T, mode: Int): Option[nodeTree[T]] = {
     var verifier: Boolean = true
     var currentNode: nodeTree[T] = root
 
     if (mode == 0) {
-      while (verifier == true) {
+      while (verifier) {
         /* Indo para a direita */
         if (func(value, currentNode.value)) {
           if (currentNode.rightNode == null) verifier = false
@@ -26,44 +26,49 @@ class binaryTree[T](func: (T, T) => Boolean) extends traitBinaryTree[T] {
           else currentNode = currentNode.leftNode
         }
       }
-      return currentNode
+      return Some(currentNode)
     }
+
     else {
-      while (verifier == true) {
+      while (verifier) {
+        println("Current Node: " + currentNode.value)
         /* Indo para a direita */
         if (func(value, currentNode.value)) {
-          if (currentNode.rightNode.value == value) verifier = false
+          if (currentNode.rightNode != null) { // Verifico se o próximo é nulo para conseguir comparar
+            if (currentNode.rightNode.value == value) verifier = false
+            else currentNode = currentNode.rightNode
+          }
           else {
-            if (currentNode.rightNode != null) currentNode = currentNode.rightNode
-            else {
-              println("there is nothing.")
-              verifier = false
-            }
+            println("there is nothing")
+            return None
+//            verifier = false
           }
         }
+
         /* Indo para a esquerda */
         else {
-          if (currentNode.leftNode.value == value) verifier = false
-          else{
-            if (currentNode.leftNode != null) currentNode = currentNode.leftNode
-            else {
-              println("there is nothing.")
-              verifier = false
-            }
+          if (currentNode.leftNode != null) {
+            if (currentNode.leftNode.value == value) verifier = false
+            else currentNode = currentNode.leftNode
+          }
+          else {
+            println("there is nothing")
+            return None
+//            verifier = false
           }
         }
       }
-      return currentNode
+      return Some(currentNode)
     }
   }
-
 
   override def insert(value: T): Unit = {
     if (_size == 0) {
       root = new nodeTree[T](value, null, null, null)
+      _size += 1
     }
     else {
-      val nodeAux: nodeTree[T] = findLeaf(value, 0)
+      val nodeAux: nodeTree[T] = findLeaf(value, 0).get
       if (func(value, nodeAux.value)) {
         nodeAux.rightNode = new nodeTree[T](value, null, null, nodeAux)
         _size += 1
@@ -81,21 +86,26 @@ class binaryTree[T](func: (T, T) => Boolean) extends traitBinaryTree[T] {
       return true
     }
     else {
-      val nodeAux: nodeTree[T] = findLeaf(value, 1)
-      if (func(value, nodeAux.value)) {
-        nodeAux.leftNode = null
-        _size -= 1
-        return true
+      if (findLeaf(value, 1) != None) {
+        val nodeAux: nodeTree[T] = findLeaf(value, 1).get
+
+        if (func(value, nodeAux.value)) {
+          nodeAux.leftNode = null
+          _size -= 1
+          return true
+        }
+        else {
+          nodeAux.rightNode = null
+          _size -= 1
+          return true
+        }
       }
-      else {
-        nodeAux.rightNode = null
-        _size -= 1
-        return true
-      }
+      else false
     }
   }
 
   override def show: Unit = {
+
 
 
 
